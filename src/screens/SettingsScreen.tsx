2094d0839ee329c75483fa7798b98408ad0f9e57
JohnLy2000 <johnly1809@gmail.com>
2024-02-11 01:56:52 -0500

    updated settings screen to use keyboard events

--- src/screens/SettingsScreen.tsx
+++ src/screens/SettingsScreen.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 import { useMainContext } from "../context/MainContext";
 import "../styles/screens/SettingsScreen.css";
@@ -13,6 +13,12 @@ const SettingsScreen = ({
   const { context, updateContext } = useMainContext();
   const { is24HourFormat } = context;
 
+  enum navIndices {
+    IDX_TIME = 0,
+  }
+
+  const [focusedIndex, setFocusedIndex] = useState(navIndices.IDX_TIME);
+
   const changeHourFormat = () => {
     updateContext({ is24HourFormat: !is24HourFormat });
   };
@@ -22,17 +28,29 @@ const SettingsScreen = ({
 
     settingsScreen!.setAttribute("close", "");
 
-    const animationDuration = 400;
+    const animationDuration = 300;
     setTimeout(() => {
       settingsScreen!.removeAttribute("close");
       onClose();
     }, animationDuration);
   };
 
+  const handleInteract = () => {
+    switch (focusedIndex) {
+      case navIndices.IDX_TIME:
+        changeHourFormat();
+        break;
+    }
+  }
+
   const handleKeyDown = (event: KeyboardEvent) => {
     switch (event.key) {
       case "Escape":
         handleClose();
+        break;
+
+      case "Enter":
+        handleInteract();
         break;
 
       default:
@@ -52,13 +70,19 @@ const SettingsScreen = ({
     return () => {
       document.removeEventListener("keydown", handleKeyDownLocal);
     };
-  }, [isActive]);
+  }, [isActive, is24HourFormat]);
 
   return (
     <dialog className={`settings-screen`} open={isActive}>
       <div className="settings-container">
         <div className="settings-time-container">
-          <button className="settings-time-item" onClick={changeHourFormat}>
+          <button 
+            className={`${
+              (focusedIndex === navIndices.IDX_TIME) ? 
+              "settings-time-item-focused" : "settings-time-item"}
+            `} 
+            autoFocus={focusedIndex === navIndices.IDX_TIME}
+          >
             <span className="settings-time-item-label">24-Hour Format</span>
             <input
               type="checkbox"
